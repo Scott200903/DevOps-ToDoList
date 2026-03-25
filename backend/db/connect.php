9fc682a6ce1c9862ce50d2821540e1d688d49a33
scott <info@adrag.de>
2026-03-25 22:36:19 +0100

    Tests aktualisiert und neue hinzugefügt

--- backend/db/connect.php
+++ backend/db/connect.php
@@ -1,10 +1,10 @@
 <?php
 function connectDB(): mysqli
 {
-  $servername = "localhost";
-  $username = "root";
-  $password = "geheim_1";
-  $dbname = "todolist";
+  $servername = getenv('DB_HOST') ?: "localhost";
+  $username   = getenv('DB_USER') ?: "root";
+  $password   = getenv('DB_PASSWORD') ?: "geheim_1";
+  $dbname     = getenv('DB_NAME') ?: "todolist";
 
   // Create connection
   $conn = new mysqli($servername, $username, $password, $dbname);
